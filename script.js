058afabbd099be2a68e404048519862f30d69fe9
bahareh-bahrami <bahareh.bahramy@gmail.com>
2025-05-05 17:05:48 +0330

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+//Selecting Elements
+const scoreEl0 = document.querySelector('#score--0');
+const scoreEl1 = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+//Start Condition
+scoreEl0.textContent = 0;
+scoreEl1.textContent = 0;
+diceEl.classList.add('hidden');
+
+//User Rolls Dice
+
+//User Holds Score
+
+//User Resets game

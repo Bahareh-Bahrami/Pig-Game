00726a7054506a8bad3a269e2cf6620831e415a4
bahareh-bahrami <bahareh.bahramy@gmail.com>
2025-05-06 16:09:20 +0330

    Final Commit

--- script.js
+++ script.js
@@ -16,12 +16,22 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //Start Condition
+playing = true;
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const totalScores = [0, 0];
 let activePlayer = 0;
 let currentScore = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //User Rolls Dice
 btnRoll.addEventListener('click', function () {
@@ -40,14 +50,40 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     //Switch player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
 });
 
 //User Holds Score
+btnHold.addEventListener('click', function () {
+  //1.Add current score to total score of active player
+  totalScores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    totalScores[activePlayer];
+
+  //2.check total score >= 100
+  if (totalScores[activePlayer] >= 100) {
+    // Finish the game
+    playing = false;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  }
+  //Switch player
+  else {
+    switchPlayer();
+  }
+});
 
 //User Resets game
+btnNew.addEventListener('click', function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+
+  let activePlayer = 0;
+  let currentScore = 0;
+});
